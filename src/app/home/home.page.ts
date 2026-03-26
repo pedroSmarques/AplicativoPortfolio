cc5852f279625f338535390456ec185112e537dd
Pedro Marques <pedromarquess2006@gmail.com>
2026-03-25 23:59:50 -0300

    Rotas ligadas para suas respectivas paginas(conteudos)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
-  constructor() {}
+    constructor(private router: Router) {}
+
+  navigateToDetails() {
+    this.router.navigate(['/sobre']);
+  }
 }
--- src/app/sobre/sobre.page.ts
+++ src/app/sobre/sobre.page.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.page.html',
   styleUrls: ['./sobre.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton ,IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class SobrePage implements OnInit {
 
-  constructor() { }
+constructor(private router: Router) {}
 
   ngOnInit() {
   }
 
+    
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
 }
