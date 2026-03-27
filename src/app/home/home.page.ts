96bf3c3c376fe1c1f9b4f245b9d5297f7b76a0e6
Pedro Marques <pedromarquess2006@gmail.com>
2026-03-26 22:19:16 -0300

    finalização da Avaliação Formadora 1, com suas rotas e eventos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonAvatar } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonButton, 
+    IonAvatar
+  ]
 })
 export class HomePage {
-    constructor(private router: Router) {}
+
+  constructor(private router: Router) {}
 
   navigateToDetails() {
     this.router.navigate(['/sobre']);
   }
+
 }
--- src/app/sobre/sobre.page.ts
+++ src/app/sobre/sobre.page.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonAvatar } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.page.html',
   styleUrls: ['./sobre.page.scss'],
   standalone: true,
-  imports: [IonButton ,IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonAvatar]
 })
 export class SobrePage implements OnInit {
 
-constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-    
+
   navigateToHome() {
     this.router.navigate(['/home']);
   }
